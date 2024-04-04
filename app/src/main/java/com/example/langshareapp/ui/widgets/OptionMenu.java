4f71242dd53965da61a11913c912451f37bae8e1
ndoniyor <ndoniyor123@gmail.com>
2024-04-04 13:53:38 -0400

    refactor: remove edit account settings

--- app/src/main/java/com/example/langshareapp/ui/widgets/OptionMenu.java
+++ app/src/main/java/com/example/langshareapp/ui/widgets/OptionMenu.java
@@ -3,16 +3,14 @@ package com.example.langshareapp.ui.widgets;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.langshareapp.R;
 
 public class OptionMenu extends LinearLayout {
-    Button editProfileButton;
-    Button accountSettingsButton;
-    Button logoutButton;
+    public Button editProfileButton;
+    public Button logoutButton;
     
     public OptionMenu(Context context) {
         super(context);
@@ -34,10 +32,6 @@ public class OptionMenu extends LinearLayout {
         inflater.inflate(R.layout.option_menu, this, true);
 
         editProfileButton = findViewById(R.id.edit_profile_button);
-        accountSettingsButton = findViewById(R.id.view_account_settings_button);
         logoutButton = findViewById(R.id.logout_button);
-
-        
-        
     }
 }

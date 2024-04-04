054fdf202974ff16df104f22cabc99aed599b319
ndoniyor <ndoniyor123@gmail.com>
2024-04-04 13:52:43 -0400

    wired edit profile and logout

--- app/src/main/java/com/example/langshareapp/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/langshareapp/ui/account/AccountFragment.java
@@ -9,9 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.langshareapp.R;
 import com.example.langshareapp.databinding.FragmentAccountBinding;
 import com.example.langshareapp.ui.widgets.OptionMenu;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountFragment extends Fragment {
 
@@ -22,10 +27,35 @@ public class AccountFragment extends Fragment {
         AccountViewModel accountViewModel =
                 new ViewModelProvider(this).get(AccountViewModel.class);
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        TextView textView = binding.helloText;
+        textView.setText("Hello, " + mAuth.getCurrentUser().getDisplayName() + "!");
+
         OptionMenu optionMenu = binding.optionMenu;
+        optionMenu.editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
+                navController.navigate(R.id.action_accountFragment_to_editProfileFragment);
+            }
+        });
+
+        optionMenu.logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(getActivity())
+                        .addOnCompleteListener(task -> {
+                            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
+                            navController.navigate(R.id.action_accountFragment_to_landingPage);
+                        });
+            }
+        });
+
         return root;
     }
 

0d2a9c9074a9786998d4416d4b93c4c7bc280427
ndoniyor <ndoniyor123@gmail.com>
2024-04-25 12:44:16 -0400

    Refactor MessageAdapter and ChatViewModel classes

--- app/src/main/java/com/example/langshareapp/adapters/messageadapter/MessageAdapter.java
+++ app/src/main/java/com/example/langshareapp/adapters/messageadapter/MessageAdapter.java
@@ -1,5 +1,6 @@
 package com.example.langshareapp.adapters.messageadapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,37 +11,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.langshareapp.R;
 import com.example.langshareapp.utils.Message;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
-public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
-    private List<Message> messageList;
+public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<Message> messageList = new ArrayList<>();
     private String currentUserId;
     private static final int VIEW_TYPE_MESSAGE_SENT = 1;
     private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
 
-    public MessageAdapter(List<Message> messageList, String currentUserId){
+    public MessageAdapter(List<Message> messageList, String currentUserId) {
         this.messageList = messageList;
         this.currentUserId = currentUserId;
-    }
-
-    @Override
-    public int getItemViewType(int position){
-        Message message = messageList.get(position);
-        if (message.getSenderId().equals(currentUserId)) {
-            return VIEW_TYPE_MESSAGE_SENT;
-        } else {
-            return VIEW_TYPE_MESSAGE_RECEIVED;
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        return messageList.size();
+        setHasStableIds(true);
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
         if (viewType == VIEW_TYPE_MESSAGE_SENT) {
             view = LayoutInflater.from(parent.getContext())
@@ -55,7 +46,7 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-Message message = messageList.get(position);
+        Message message = messageList.get(position);
         switch (holder.getItemViewType()) {
             case VIEW_TYPE_MESSAGE_SENT:
                 ((SentMessageHolder) holder).bind(message);
@@ -63,6 +54,39 @@ Message message = messageList.get(position);
             case VIEW_TYPE_MESSAGE_RECEIVED:
                 ((ReceivedMessageHolder) holder).bind(message);
         }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messageList.get(position);
+        if (message.getSenderId().equals(currentUserId)) {
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList == null ? 0 : messageList.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return messageList.get(position).getMessageId();
+    }
+
+    public void updateMessages(HashMap<String, Message> newMessages) {
+        messageList = new ArrayList<>(newMessages.values());
+        messageList.sort(new Comparator<Message>() {
+            @Override
+            public int compare(Message m1, Message m2) {
+                int result = Integer.compare(m1.getMessageId(), m2.getMessageId());
+                Log.i("FirebaseRTDatabaseRepo", "Comparing messages  " + m1.getContent() + " - " + m1.getMessageId() + " and " + m2.getContent() + " - " + m2.getMessageId() + " result: " + result);
+                return result;
+            }
+        });
+        notifyDataSetChanged();
     }
 
 }
--- app/src/main/java/com/example/langshareapp/viewmodels/ChatViewModel.java
+++ app/src/main/java/com/example/langshareapp/viewmodels/ChatViewModel.java
@@ -1,22 +1,58 @@
 package com.example.langshareapp.viewmodels;
 
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.langshareapp.repositories.FirebaseFirestoreRepository;
 import com.example.langshareapp.repositories.FirebaseRTDatabaseRepository;
+import com.example.langshareapp.utils.LangShareUser;
 import com.example.langshareapp.utils.Message;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class ChatViewModel extends ViewModel{
     private FirebaseRTDatabaseRepository mRepository;
+    private FirebaseFirestoreRepository mFirestoreRepository;
+    private MutableLiveData<HashMap<String, Message>> mMessages;
+    private LangShareUser mUser;
 
-    public ChatViewModel(String senderId, String receiverId){
+    public ChatViewModel(String senderId, String receiverId, String chatRoomId){
         mRepository = new FirebaseRTDatabaseRepository(senderId, receiverId);
-        mRepository.createChatRoom();
+        mFirestoreRepository = FirebaseFirestoreRepository.getInstance();
+
+        final String chatId;
+        if(chatRoomId.isEmpty()){
+            chatId = mRepository.createChatRoom();
+        } else {
+            chatId = chatRoomId;
+            mRepository.connectToChatRoom(chatId);
+        }
+
+        mFirestoreRepository.getCurrentUser(
+                user -> {
+                    mUser = user;
+                    mUser.addChatHistory(chatId);
+                    mFirestoreRepository.addConversationToHistory(mUser.getAuthId(), chatId);
+                }
+        );
+
+        mMessages = new MutableLiveData<>();
+
+        mRepository.addChatRoomUpdateListener(chatRoom -> {
+            mMessages.postValue(chatRoom.getMessages());
+        });
     }
 
     public void sendMessage(String senderId, String receiverId, String content, long timestamp){
-        Message message = new Message(senderId, receiverId, content, timestamp);
+        int matchId = mMessages.getValue() != null ? mMessages.getValue().size() : 0;
+        Message message = new Message(matchId, senderId, receiverId, content, timestamp);
         mRepository.sendMessage(message);
     }
 
+    public LiveData<HashMap<String, Message>> getMessages(){
+        return mMessages;
+    }
 }
